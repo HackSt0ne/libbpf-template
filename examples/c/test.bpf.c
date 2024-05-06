cf2383c98df254208e310a6560663763b88a7534
st0ne <2214167862@qq.com>
2024-05-07 01:14:44 +0800

    tmplate

---
+++ examples/c/test.bpf.c
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
+/* Copyright (c) 2020 Facebook */
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
+int my_pid = 0;
+int g_test = 123;
+
+SEC("tp/syscalls/sys_enter_write")
+int handle_tp(void *ctx)
+{
+	int pid = bpf_get_current_pid_tgid() >> 32;
+
+	if (pid != my_pid)
+		return 0;
+
+	bpf_printk("test BPF triggered enter from PID %d.\n", pid);
+
+	return 0;
+}
+
+
+SEC("tp/syscalls/sys_exit_write")
+int handle_tp_exit(void *ctx)
+{
+	int pid = bpf_get_current_pid_tgid() >> 32;
+
+	if (pid != my_pid)
+		return 0;
+
+	bpf_printk("BPF triggered exit from PID %d.\n", pid);
+
+	return 0;
+}
